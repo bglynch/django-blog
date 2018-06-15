c89e09b2d391dfee7da404d3bce1eb1e0330ce5a
Brian Lynch <bglynch17@gmail.com>
2018-06-15 13:42:22 +0000

    added unique page for every blog post

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 # posts/urls.py
 
 from django.urls import path
-from .views import get_posts
+from .views import get_posts, post_detail
 
 urlpatterns = [
+    path('<pk>/', post_detail, name='post_detail'),
     path('', get_posts, name='get_posts'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,19 @@
 # posts/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
 def get_posts(request):
     posts = Post.objects.all()
     return render(request, "posts/blogposts.html", {'posts': posts})
+    
+def post_detail(request, pk):
+    '''
+    Create a view tthat returns a single Post object based on the Post ID (pk) and 
+    render it to the 'postdetail.html' template
+    '''
+    post = get_object_or_404(Post, pk=pk)
+    post.views += 1
+    post.save()
+    return render(request, "posts/postdetail.html", {'post': post})
