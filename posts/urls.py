f5dadb6d1c94dce01d489403569d57da8349f34b
Brian Lynch <bglynch17@gmail.com>
2018-06-15 14:51:23 +0000

    add ability to edit a blog post

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 # posts/urls.py
 
 from django.urls import path
-from .views import get_posts, post_detail
+from .views import get_posts, post_detail, create_or_edit_post
 
 urlpatterns = [
+    path('<pk>/edit', create_or_edit_post, name='edit_post'),
     path('<pk>/', post_detail, name='post_detail'),
     path('', get_posts, name='get_posts'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import BlogPostForm
 
 # Create your views here.
 def get_posts(request):
@@ -17,3 +18,20 @@ def post_detail(request, pk):
     post.views += 1
     post.save()
     return render(request, "posts/postdetail.html", {'post': post})
+    
+def create_or_edit_post(request, pk=None):
+    """
+    Create a view that allows us to create
+    or edit a post depending if the Post ID
+    is null or not
+    """
+    post = get_object_or_404(Post, pk=pk) if pk else None
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post_detail, post.pk)
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'posts/blogpostform.html', {'form': form})
+    
