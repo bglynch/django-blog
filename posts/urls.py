eeb77d494c431a917a125684f568a158d99f8239
Brian Lynch <bglynch17@gmail.com>
2018-06-18 14:17:57 +0000

    create_edit post function split into 2 functions

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,11 @@
 # posts/urls.py
 
 from django.urls import path
-from .views import get_posts, post_detail, create_or_edit_post
+from .views import get_posts, post_detail, new_post, edit_post
 
 urlpatterns = [
-    path('new_post', create_or_edit_post, name='new_post'),
-    path('<int:pk>/edit', create_or_edit_post, name='edit_post'),
+    path('new_post', new_post, name='new_post'),
+    path('<int:pk>/edit', edit_post, name='edit_post'),
     path('<int:pk>/', post_detail, name='post_detail'),
     path('', get_posts, name='get_posts'),
 ]
